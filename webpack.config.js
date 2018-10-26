91b693c4fc634294e9401be72525994c658ed5e3
tanghuiming <thm133@foxmail.com>
2018-10-26 19:44:04 +0800

    webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,26 @@
 'use strict'
 const path = require('path')
 const VueLoaderPlugin = require('vue-loader/lib/plugin')
-module.exports = {
+const HTMLPlugin = require('html-webpack-plugin')
+const webpack = require('webpack')
+
+const isDev = process.env.NODE_ENV === 'development'
+
+const config = {
+    target: 'web',
     entry: path.join(__dirname,'src/index.js'),
     output:{
         filename: 'bundle.js',
         path: path.join(__dirname,'dist')
     },
     plugins:[
-        new VueLoaderPlugin()
+        new webpack.DefinePlugin({
+            'process.env':{
+                NODE_ENV: isDev ? '"development"':'"production"'
+            }
+        }),
+        new VueLoaderPlugin(),
+        new HTMLPlugin()
     ],
     module:{
         rules:[
@@ -43,3 +55,23 @@ module.exports = {
         ]
     }
 }
+
+
+if(isDev){
+    config.devtool = '#cheap-module-eval-source-map'
+    config.devServer = {
+        port: 8000,
+        host: '0.0.0.0',
+        overlay:{
+            errors:true
+        },
+        open: true,
+        hot: true
+    }
+    config.plugins.push(
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoEmitOnErrorsPlugin()
+    )
+}
+
+module.exports = config
